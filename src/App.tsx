f0eddd0807040ea87947b6c7dc82206557bf1f79
Adenuga Oluwatunmise Wilson <>
2022-01-14 04:33:47 +0100

    chore: add teeter totter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import TeeterTotter from './containers/teeterTotter/index';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='sr-only'>Teeter Totter Game</h1>
+      <main className='App-body'>
+        <TeeterTotter />
+      </main>
     </div>
   );
 }
